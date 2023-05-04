fcfdaa79ec40b43404ff32c0e1c2fb1b992752dd
Robert Snell <91638194+roberts1y@users.noreply.github.com>
2023-05-04 14:23:34 -0400

    feat: update to 2023.4.3 & remove balancing for template repo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,21 +4,19 @@ import frc.robot.Constants.OIConstants;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.BalanceControlCommand;
 import frc.robot.commands.SwerveCommand;
 import frc.robot.commands.SwitchFunction;
 import frc.robot.subsystems.SwerveDriveSubsystem;
 import frc.robot.Constants.OIConstants;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.BalanceControlCommand;
 
 
 public class RobotContainer {
 
    
     private final SwerveDriveSubsystem swerveSubsystem = new SwerveDriveSubsystem();
-    public Command m_balance;
+
     private final Joystick driverJoystick = new Joystick(OIConstants.kDriverJoystickPort);
     private JoystickButton buttonX = new JoystickButton(driverJoystick, 3);
     private JoystickButton buttonY = new JoystickButton(driverJoystick, 4);
@@ -39,14 +37,6 @@ public class RobotContainer {
     
 
     private void configureButtonBindings() {
-        buttonX.onTrue(new InstantCommand(){
-            public void initialize() {
-                System.out.println("Button X pressed");
-                m_balance = new BalanceControlCommand(swerveSubsystem);
-                m_balance.schedule();
-            }
-        }
-        ); 
 
     }
 }
